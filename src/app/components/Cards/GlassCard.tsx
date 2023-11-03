547fce73243161923ad04d67fa0d4b87ea2d4e17
Harry Webb <harrywebb21@gmail.com>
2023-11-03 15:56:45 +0000

    more changes

--- src/app/components/Cards/GlassCard.tsx
+++ src/app/components/Cards/GlassCard.tsx
@@ -8,7 +8,7 @@ export default function GlassCard({
   return (
     <>
       <div
-        className={`${className} rounded-2xl border border-white/50 w-100 h-[90vh]`}
+        className={`${className} rounded-2xl border border-white/50 w-100 h-[80vh] sm:h-[90vh]`}
       >
         {children}
       </div>
