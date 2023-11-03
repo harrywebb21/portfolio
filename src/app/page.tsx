d3fc8b6a7797cc623e47451df77e0c8e71d87500
harrywebb21 <harrywebb21@gmail.com>
2023-11-03 05:01:17 +0000

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,7 @@ export default function Home() {
         </div>
         <GradientButton className={robotoFlex.className} buttonName="ACCESS" />
         <GlowLine />
+        test
       </GlassCard>
     </>
   );
