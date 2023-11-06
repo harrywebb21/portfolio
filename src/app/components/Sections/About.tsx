4621b2e01d74b54239f547aa92845075515d083f
Harry Webb <harrywebb21@gmail.com>
2023-11-06 12:20:27 +0000

    checking how mobile looks on device

---
+++ src/app/components/Sections/About.tsx
@@ -0,0 +1,13 @@
+import { squadaOne } from "@/app/fonts/fonts";
+
+export default function About() {
+  return (
+    <div className="">
+      <p
+        className={`${squadaOne.className} text-8xl text-white sm:text-[256px]`}
+      >
+        HEY👋🏼
+      </p>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import About from "./components/Sections/About";
 import Hero from "./components/Sections/Hero";
 
 export default function Home() {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }
