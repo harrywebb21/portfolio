f4bcd4c642a878e45c385443cd00feaab45a5645
Harry Webb <harrywebb21@gmail.com>
2023-11-13 23:22:19 +0000

    updating nextjs issues

--- src/app/projects/page.tsx
+++
@@ -1,9 +0,0 @@
-import BGGlassCard from "../components/Cards/GlassCard";
-
-export default function projects() {
-  return (
-    <BGGlassCard className="absolute top-0 z-10 flex items-center justify-center">
-      <h1 className="text-4xl text-white font-bold bg-transparent">Projects</h1>
-    </BGGlassCard>
-  );
-}
